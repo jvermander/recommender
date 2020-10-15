a6ef9e47a58b240b2bccce09730e6ac2d126889e
leo <leo@rampart.gateway.mts.net>
2020-10-15 00:48:26 -0500

    Replace PySpark with Implicit; add author-based recommendation

--- src/recommender.py
+++ src/recommender.py
@@ -7,96 +7,195 @@ import pandas as pd
 import scipy.sparse as sps
 from scipy.sparse import csr_matrix
 
-import pyspark
-from pyspark.sql import SparkSession
-from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.recommendation import ALS, ALSModel
-from pyspark.ml.tuning import CrossValidator, ParamGridBuilder, CrossValidatorModel
-
-from sklearn.model_selection import GridSearchCV, cross_val_score, KFold, StratifiedKFold, RandomizedSearchCV
+import implicit
+from sklearn.model_selection import GridSearchCV, cross_val_score, KFold, StratifiedKFold, RandomizedSearchCV, train_test_split
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics.pairwise import cosine_similarity
+
+import pickle
 
 import csv
 import time
 import os
 import glob
 
-
 TRAIN_DIR = 'data/train10'
-BOOKS_CSV = 'data/lookup.csv'
+BOOKS_CSV = 'data/booksdataset.csv'
+AUTHS_CSV = 'data/authsdataset.csv'
 
+user = 6589
 def main():
-
-  # data = pd.read_csv('data/out.csv')
-
-  # by_item = data.groupby('BID')['UID'].nunique()
-  # by_item = by_item[by_item >= 5]
-  # print(by_item)
-
-  # data = data[data.BID.isin(by_item.index)]
-  # print(data.nunique())
-
-  # data = lower_bound_ratings(data, 10, 'data/b5u10.csv')
-
-  # print(data.nunique())
-
-  # print(data)
-
-  files = glob.glob(TRAIN_DIR + '/*.csv')
-  df = pd.concat((pd.read_csv(f) for f in files), ignore_index=True)
-  df.sort_values('UID', inplace=True)
-  df.reset_index(drop=True, inplace=True)
-  # cross_val(df.copy())
-  train_model('models/ALS', df.copy(), offset=False)
-  model, mu_i, mu_u = load_model('models/ALS')
-  recommend(model, df, mu_i, mu_u)
-
-def train_model( path, df, offset ):
-  os.environ["PYSPARK_PYTHON"]="/usr/bin/python3.5"
-  ps = SparkSession.builder.master("local[4]").appName('Recommender').getOrCreate()
+  # files = glob.glob(TRAIN_DIR + '/*.csv')
+  # df = pd.concat((pd.read_csv(f) for f in files), ignore_index=True)
+  # df.sort_values('UID', inplace=True)
+  # df.reset_index(drop=True, inplace=True)
   
-  df, mu_i = normalize_by_item(df)
-  mu_u = None
-  if(offset):
-    df, mu_u = normalize_by_user(df)
-  df = ps.createDataFrame(df)
-  als = ALS(userCol='UID', itemCol='BID', ratingCol='Score', coldStartStrategy='drop',
-            maxIter=15, rank=20, regParam=0.3)
-  model = als.fit(df)
-  model.write().overwrite().save(path)
-  np.save(path + '/mu_item.npy', mu_i)
-  if(offset):
-    np.save(path + '/mu_user.npy', mu_u)
-  return model, mu_i, mu_u
+  # df = pd.DataFrame(df.groupby(['UID', 'AID']).Score.mean())
+  # df.reset_index(inplace=True)
+  # df.rename(columns={'AID':'BID'}, inplace=True)
+  # df.to_csv(TRAIN_DIR + 'authors10.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
+
+  books = pd.read_csv(TRAIN_DIR + '/books10.csv')
+  auths = pd.read_csv(TRAIN_DIR + '/authors10.csv')
+
+  path = 'models/impALS'
+  # # train_model(books, auths, path)
+  book_model, auth_model = load_model(path)
+  recommend(books, auths, book_model, auth_model)
+
+def train_model( books, authors, path, f=200, l=0.3 ):
+
+
+  model = implicit.als.AlternatingLeastSquares(factors=f, regularization=l, num_threads=4)
+  csr = books_to_csr(books)
+  model.fit(csr.transpose())
+  pickle.dump(model, open(path + '/impALSbooks.sav', 'wb'))
+
+  model = implicit.als.AlternatingLeastSquares(factors=f, regularization=l, num_threads=4)
+  csr = authors_to_csr(authors)
+  model.fit(csr.transpose())
+  pickle.dump(model, open(path + '/impALSauthors.sav', 'wb'))
 
 def load_model( path ):
-  os.environ["PYSPARK_PYTHON"]="/usr/bin/python3.5"
-  ps = SparkSession.builder.master("local[4]").appName('Recommender').getOrCreate()
+  book_model = pickle.load(open(path + '/impALSbooks.sav', 'rb'))
+  auth_model = pickle.load(open(path + '/impALSauthors.sav', 'rb'))
+  
+  return book_model, auth_model
 
-  model = ALSModel.load(path)
-  mu_i = np.load(path + '/mu_item.npy')
-  try:
-    mu_u = np.load(path + '/mu_user.npy')
-  except:
-    mu_u = None
-  return model, mu_i, mu_u
+# todo: finish author recommendations, consolidate data, clean-up
 
-def lookup( ratings, labels=['UID', 'BID', 'ISBN', 'Book-Title', 'Book-Author', 'Score'] ):
+def recommend( books, auths, book_model, auth_model ):
+  global user
+
+  print('UID ', user)
+  user_ratings = get_user_ratings(user, books)
+  print(user_ratings)
+
+  # 1) user-user similarity
+  # 2) item-item similarity
+  # 3) rank by book score, author score, frequency
+
+  candidates = pd.Series(name='BID')
+  candidates = candidates.append(recommend_by_book(user, book_model, books))
+  candidates = candidates.append(recommend_by_user(user, book_model, books))
+  candidates = remove_already_read(user_ratings, candidates)
+
+  book_scores = score_by_book(user, book_model, books, candidates)
+  aids = lookup_books(candidates).AID
+  auth_scores = score_by_author(user, auth_model, auths, aids)
+
+  ranking = rank_books(book_scores, auth_scores)
+  ranking.sort_values(['Count', 'Overall'], ascending=False, inplace=True)
+  # ranking = filter_ranking(ranking)
+
+  print(ranking[:30])
+
+
+  aids = recommend_by_author(user, auth_model, auths).sort_values('AuthScore', ascending=False)
+  author_ranking = lookup_authors(aids)
+  author_ranking.drop_duplicates('AID', inplace=True)
+  print(author_ranking)
+
+
+def lookup_books( bids ):
   lookup = pd.read_csv(BOOKS_CSV, sep=';', error_bad_lines=False)
-  ratings = ratings.merge(lookup, on='BID', how='left')
-  ratings = ratings.loc[:, labels]
-  return ratings
+  bids = pd.DataFrame(bids)
+  summary = bids.merge(lookup, on='BID', how='left')
+  summary.drop(inplace=True, 
+    columns=['Year-Of-Publication', 'Publisher', 'Image-URL-S', 'Image-URL-M', 'Image-URL-L'])
+  return summary
+
+def lookup_authors( aids ):
+  lookup = pd.read_csv(AUTHS_CSV, sep=',', error_bad_lines=False)
+  aids = pd.DataFrame(aids)
+  summary = aids.merge(lookup, on='AID', how='left')
+  return summary
 
 def get_user_ratings( user, ratings ):
-  user_ratings = lookup(ratings[ratings.UID == user])
+  user_ratings = lookup_books(ratings[ratings.UID == user])
   return user_ratings
+
+def recommend_by_book( user, model, ratings, thres=4.0, n=5 ):
+  bids = ratings[(ratings.UID == user)]
+  bids = bids[bids.Score >= thres].BID
+  bids.reset_index(drop=True, inplace=True)
+
+  result = []
+  csr = books_to_csr(ratings)
+  for i in range(bids.shape[0]):
+    result += (model.similar_items(bids[i], N=n, react_users=csr.transpose()))
+  result = [x for x,_ in result]
+  result = pd.Series(result)
+  result.name = 'BID'
+  return result
+
+def recommend_by_user( user, model, ratings, thres=4.0, n=5 ):
+  ids_tuples = model.similar_users(user, n)
+  ids = [x for x, _ in ids_tuples]
+
+  bids = ratings[ratings.UID.isin(ids)]
+  bids = bids[bids.Score >= thres].BID
+  bids.reset_index(drop=True, inplace=True)
+
+  return bids
+
+#todo
+def recommend_by_author( user, model, ratings, thres=4.0, n=5 ):
+  aids = ratings[(ratings.UID == user)]
+  aids = aids[aids.Score >= thres].AID
+  aids.reset_index(drop=True, inplace=True)
+
+  result = []
+  csr = authors_to_csr(ratings)
+  for i in range(aids.shape[0]):
+    result += (model.similar_items(aids[i], N=n, react_users=csr.transpose()))
+  # result = [x for x,_ in result]
+  result = pd.DataFrame(result)
+  result.columns = ['AID', 'AuthScore']
+  return result
+
+
+def remove_already_read( user_ratings, bids ):
+  bids = bids[~bids.isin(user_ratings.BID)]
+  return bids
+
+def score_by_book( user, model, ratings, bids ):
+  csr = books_to_csr(ratings)
+  scores = model.rank_items(user, csr, bids.to_list())
+  scores = pd.DataFrame(scores, columns=['BID', 'BookScore'])
+
+  return scores
+
+def score_by_author( user, model, ratings, aids ):
+  csr = authors_to_csr(ratings)
+  scores = model.rank_items(user, csr, aids.to_list())
+  scores = pd.DataFrame(scores, columns=['AID', 'AuthScore'])
+
+  return scores
+
+def rank_books( book_scores, auth_scores ):
+  count = book_scores.groupby('BID').count()
+  count.columns = ['Count']
+  book_scores.drop_duplicates('BID', inplace=True)
+  auth_scores.drop_duplicates('AID', inplace=True)
+
+  ranking = lookup_books(book_scores)
+  ranking = ranking.merge(auth_scores, on='AID', how='left')
+  ranking = ranking.merge(count, on='BID', how='left')
+  
+  ranking['Overall'] = ranking['BookScore'] + 1.2 * ranking['AuthScore']
+  ranking = ranking[['BID', 'AID', 'ISBN', 'Book-Title', 'Book-Author', \
+                     'Count', 'BookScore', 'AuthScore', 'Overall']]
+  return ranking
+
+# def filter_ranking( ranking ):
+#   ranking = ranking[ranking.Overall >= 0.03]
+
 
 def get_similar_users( user, ratings, n=5 ):
   liked = ratings.Score >= 4.0
   # ratings.loc[liked, 'Score'] = 1
   ratings.loc[~liked, 'Score'] = 0
-  
 
   csr = get_csr(ratings)
   similarities = pd.Series(cosine_similarity(csr, csr[user, :]).reshape(-1))
@@ -104,13 +203,18 @@ def get_similar_users( user, ratings, n=5 ):
   ids = similarities.index[1:1+n]
 
   return ids
-  
-def get_similar_items( user_ratings, ratings, n_user='all', n_similar=5 ):
-  user_ratings.sort_values('Score', ascending=False, inplace=True)
-  if(n_user == 'all'):
-    top_ratings = user_ratings
-  else:
-    top_ratings = user_ratings[:n_user]
+
+def get_similar_user_ratings( user, ratings, n=5 ):
+  ids = get_similar_users(user, ratings, n)
+  sim = lookup_books(ratings[ratings.UID.isin(ids)])
+
+  return sim.BID
+
+
+
+def get_similar_items( user_ratings, ratings, thres=4.0, n_similar=5 ):
+  # user_ratings.sort_values('Score', ascending=False, inplace=True)
+  top_ratings = user_ratings[user_ratings.Score >= thres]
   csr = get_csr(ratings).transpose()
   similarities = pd.DataFrame(cosine_similarity(csr, csr[top_ratings.BID]))
 
@@ -118,7 +222,7 @@ def get_similar_items( user_ratings, ratings, n_user='all', n_similar=5 ):
   ids = pd.DataFrame(ids.flatten())
   ids.columns = ['BID']
   ids = ids[~ids.BID.isin(user_ratings.BID)]
-  items = lookup(ids, ['BID', 'ISBN', 'Book-Title', 'Book-Author'])
+  items = lookup_books(ids, ['BID', 'ISBN', 'Book-Title', 'Book-Author'])
 
   counts = pd.DataFrame(items.groupby('BID').count().iloc[:, 0])
   counts.columns = ['Count']
@@ -126,127 +230,10 @@ def get_similar_items( user_ratings, ratings, n_user='all', n_similar=5 ):
   items.drop_duplicates('BID', inplace=True)
   # items.sort_values('Count', inplace=True, ascending=False)
 
-  return items, counts
-
-def get_similar_user_ratings( user, ratings, n=5 ):
-  ids = get_similar_users(user, ratings, n)
-  sim = lookup(ratings[ratings.UID.isin(ids)])
-  counts = pd.DataFrame(sim.groupby('BID').UID.count())
-  counts.columns = ['Count']
-
-  return sim, counts
-
-def predict( user_vector, items_pd, mu_i, user_offset=0 ):
-  items_np = np.stack(items_pd.features)
-  user_vector = user_vector.reshape(-1)
-  p = pd.DataFrame(np.dot(items_np, user_vector))
-  p.columns = ['Predicted']
-  p = pd.concat([items_pd.reset_index(drop=True), p.reset_index(drop=True)], axis=1)
-  p.rename(columns={'id': 'BID'}, inplace=True)
-  p = lookup(p, ['BID', 'ISBN', 'Book-Title', 'Book-Author', 'Predicted'])
-  p.Predicted += mu_i[p.BID] + user_offset
-  p.sort_values('Predicted', axis=0, ascending=False, inplace=True)
-  p.reset_index(drop=True, inplace=True)
-  return p
-
-
-
-
-# todo: featurize author, clean-up
-
-def recommend( model, ratings, mu_i, mu_u=None ):
-  user = ratings.UID.nunique()-1
-  # user = 7756
-  lookup = pd.read_csv(BOOKS_CSV, sep=';', error_bad_lines=False)
-
-  print(user)
-  user_ratings = get_user_ratings(user, ratings.copy())
-  print(user_ratings)
-
-
-  similar_items, item_counts = get_similar_items(user_ratings, ratings.copy(), 'all', 10)
-
-  similar_users = get_similar_users(user, ratings)
-  similar_ratings, counts = get_similar_user_ratings(user, ratings.copy(), 10)
-
-  items_pd = model.itemFactors.toPandas()
-  users_pd = model.userFactors.toPandas()
-  user_vec = np.stack(users_pd[users_pd.id == user].features)
- 
-  # items
-  similar_pd = items_pd[items_pd.id.isin(similar_items.BID)]
-  user_offset = 0
-  if(mu_u is not None):
-    user_offset = mu_u[user]
-  similar_items_predictions = predict(user_vec, similar_pd, mu_i, user_offset)
-  similar_items_predictions = similar_items_predictions.merge(item_counts, on='BID', how='left')
-  similar_items_predictions.sort_values(['Count', 'Predicted'], inplace=True, ascending=False)
-  print(similar_items_predictions[:30])
-
-  # users
-  similar_pd = items_pd[items_pd.id.isin(similar_ratings.BID)]
-  user_offset = 0
-  if(mu_u is not None):
-    user_offset = mu_u[user]
-  similar_predictions = predict(user_vec, similar_pd, mu_i, user_offset)
-  similar_predictions = similar_predictions.merge(counts, on='BID', how='left')
-  similar_predictions.sort_values(['Count', 'Predicted'], inplace=True, ascending=False)
-  print(similar_predictions[~similar_predictions.BID.isin(user_ratings.BID)][:30])
-
-  all = similar_items_predictions.merge(similar_predictions, on='BID')
-  print(all)
-  p = predict(user_vec, items_pd, mu_i, user_offset)
-  # print(p.iloc[:10, :])
-  # print(p[p.BID == 2809])
-  print(p[p.BID.isin(user_ratings.BID)])
-
-# Tunes hyperparameters and evaluates the model building process
-def cross_val( df ):
-  df, mu_i, mu_u = normalize_by_item_and_user(df)
-  os.environ["PYSPARK_PYTHON"]="/usr/bin/python3.5"
-  ps = SparkSession.builder.master("local[4]").appName('Recommender').getOrCreate()
-  x = ps.createDataFrame(df)
-  outer_train, outer_test = x.randomSplit([0.95, 0.05])
-  als = ALS(userCol='UID', 
-            itemCol='BID', 
-            ratingCol='Score', 
-            coldStartStrategy='drop',
-            maxIter=15)
-  grid = ParamGridBuilder().addGrid(als.rank, [10, 20, 30],) \
-                           .addGrid(als.regParam, [0.3, 0.6]).build()
-  evaluator = RegressionEvaluator(metricName='rmse', 
-                                 labelCol='Score', 
-                                 predictionCol='prediction')
-  inner_cv = CrossValidator(estimator=als, 
-                            estimatorParamMaps=grid, 
-                            evaluator=evaluator,
-                            numFolds=3,
-                            seed=1)
-
-  cv_model = inner_cv.fit(outer_train)
-
-  scores = cv_model.avgMetrics
-  print(scores)
-  for i in range(len(scores)):
-    print(grid[i].values(), scores[i])
-
-  predictions = cv_model.transform(outer_test)
-  rmse = evaluator.evaluate(predictions)
-  print("Outer: %.3f" % (float(rmse)))
-
-  predictions = predictions.toPandas()
-  outer_test = outer_test.toPandas()
-  print("Cold Start - Predicted %d out of %d - %.3f" 
-    % (predictions.shape[0], outer_test.shape[0], predictions.shape[0] / outer_test.shape[0]))
-
-  predictions_train = cv_model.transform(outer_train)
-  rmse = evaluator.evaluate(predictions_train)
-  print("Train: %.3f" % (float(rmse)))
-
-  return predictions
+  return items.BID
 
 # Assemble a sparse matrix from a dataframe
-def get_csr( ratings ):
+def books_to_csr( ratings ):
   lookup = pd.read_csv(BOOKS_CSV, sep=';', error_bad_lines=False)
 
   n_users = ratings['UID'].nunique()
@@ -259,36 +246,18 @@ def get_csr( ratings ):
   csr = csr_matrix((data, (row_ind, col_ind)), shape=(n_users, n_books))
   return csr
 
-def normalize_by_item_and_user( df ):
-  df, mu_i = normalize_by_item(df)
-  df, mu_u = normalize_by_user(df)
-  return df, mu_i, mu_u
+def authors_to_csr( ratings ):
+  lookup = pd.read_csv(BOOKS_CSV, sep=';', error_bad_lines=False)
 
-# For each rating, subtracts the average rating for the associated item (column).
-# Returns the vector of averages for later prediction
-def normalize_by_item( df ):
-  csr = get_csr(df)
+  n_users = ratings['UID'].nunique()
+  n_auth = lookup['AID'].nunique()
 
-  sums = csr.sum(axis=0).flatten()
-  counts = csr.getnnz(axis=0).flatten()
-  mu = np.divide(sums, counts, out=np.zeros_like(sums), where=(counts != 0))
-  mu = np.array(mu).flatten()
+  data = ratings['Score']
+  row_ind = ratings['UID']
+  col_ind = ratings['AID']
 
-  df.Score -= mu[df.BID]
-  return df, mu
-
-# For each rating, subtracts the average rating from the associated user (row).
-# Returns the vector of averages for later prediction
-def normalize_by_user( df ):
-  csr = get_csr(df)
-
-  sums = csr.sum(axis=1).flatten()
-  counts = csr.getnnz(axis=1).flatten()
-  mu = np.divide(sums, counts, out=np.zeros_like(sums), where=(counts != 0))
-  mu = np.array(mu).flatten()
-
-  df.Score -= mu[df.UID]
-  return df, mu
+  csr = csr_matrix((data, (row_ind, col_ind)), shape=(n_users, n_auth))
+  return csr
 
 def remove_dup_rows( df ):
   df.drop_duplicates(inplace=True)
@@ -326,6 +295,37 @@ def lower_bound_ratings( ratings, k, path ):
   ratings.to_csv(path, sep=',', quoting=csv.QUOTE_NONNUMERIC, index=False)
   return ratings
 
+
+def normalize_by_item_and_user( df ):
+  df, mu_i = normalize_by_item(df)
+  df, mu_u = normalize_by_user(df)
+  return df, mu_i, mu_u
+
+# For each rating, subtracts the average rating for the associated item (column).
+# Returns the vector of averages for later prediction
+def normalize_by_item( df ):
+  csr = get_csr(df)
+
+  sums = csr.sum(axis=0).flatten()
+  counts = csr.getnnz(axis=0).flatten()
+  mu = np.divide(sums, counts, out=np.zeros_like(sums), where=(counts != 0))
+  mu = np.array(mu).flatten()
+
+  df.Score -= mu[df.BID]
+  return df, mu
+
+# For each rating, subtracts the average rating from the associated user (row).
+# Returns the vector of averages for later prediction
+def normalize_by_user( df ):
+  csr = get_csr(df)
+
+  sums = csr.sum(axis=1).flatten()
+  counts = csr.getnnz(axis=1).flatten()
+  mu = np.divide(sums, counts, out=np.zeros_like(sums), where=(counts != 0))
+  mu = np.array(mu).flatten()
+
+  df.Score -= mu[df.UID]
+  return df, mu
 
 
 
